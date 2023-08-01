82a523dd9603f8b41f0d118745c1b25aeb4386df
Ece Aslan <eceaslan296@gmail.com>
2023-08-01 23:57:51 +0300

    Proje dosyası ekle.

---
+++ InspimoAjax/DAL/Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace InspimoAjax.DAL
+{
+    public class Context: DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server= DESKTOP-RCI54VP; initial catalog= Ajax.Inspimo; integrated security= true");
+        }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
---
+++ InspimoAjax/DAL/Customer.cs
@@ -0,0 +1,11 @@
+﻿namespace InspimoAjax.DAL
+{
+    public class Customer
+    {
+        public int CustomerID { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerSurname { get; set; }
+        public string CustomerCity { get; set; }
+
+    }
+}
